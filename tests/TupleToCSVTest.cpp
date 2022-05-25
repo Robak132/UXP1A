34fafabbc4b09f472efcad563ead1d9fc4e85e66
Paweł Müller <pawelmuller42@gmail.com>
2022-05-25 14:48:13 +0200

    Added another test for Tuple.toCSV

--- tests/TupleToCSVTest.cpp
+++ tests/TupleToCSVTest.cpp
@@ -14,3 +14,18 @@ TEST_CASE("Check toCSV()") {
     REQUIRE(result == correctCSV);
 }
 
+TEST_CASE("Check toCSV() 2") {
+    Tuple tuple = Tuple(std::vector<Entity> {
+            Entity::createIntEntity(1),
+            Entity::createStringEntity("How much wood would a woodchuck chuck?"),
+            Entity::createStringEntity("2,200"),
+            Entity::createStringEntity("Defenestration"),
+            Entity::createDoubleEntity(3.14159),
+            Entity::createIntEntity(40000000),
+    });
+
+    std::string result = tuple.toCSV();
+    std::string correctCSV = R"(1,"How much wood would a woodchuck chuck?","2,200","Defenestration",3.14159,40000000)";
+    REQUIRE(result == correctCSV);
+}
+
