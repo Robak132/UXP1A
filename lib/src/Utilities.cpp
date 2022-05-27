548c928bce54ba7572b9f16cb78b4d6da70aafad
Paweł Müller <pawelmuller42@gmail.com>
2022-05-27 19:36:26 +0200

    Added new test cases for splitString method, improved other tests.

--- lib/src/Utilities.cpp
+++ lib/src/Utilities.cpp
@@ -1,12 +1,23 @@
 #include "../include/Utilities.h"
-#include "string"
+#include <map>
+
+
+const std::map<std::string, int> LINE_ENDINGS {
+        {"\n", 0},
+        {"\r", 0},
+        {"\n\r", 0},
+        {"\r\n", 0},
+};
+
 
 std::vector<std::string> Utilities::splitString(const std::string& input) {
     std::string temp;
     std::vector<std::string> result;
 
-    for (char character : input) {
-        if (character != '\n' && character != '\r') {
+    for (const char character : input) {
+        std::string string_character;
+        string_character = character;
+        if (!LINE_ENDINGS.contains(string_character)) {
             temp += character;
         } else {
             if (!temp.empty()) {
