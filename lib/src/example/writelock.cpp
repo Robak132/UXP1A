cb746d9434fe7fd04d725cd66092cbdfa2e3c6fd
ltopolsk <lukasz.topolski00@gmail.com>
2022-05-27 10:53:59 +0200

    error undefined reference to

--- lib/src/example/writelock.cpp
+++ lib/src/example/writelock.cpp
@@ -2,29 +2,54 @@
 #include <fcntl.h>
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <string>
 
 int main() {
     int fd;
     struct flock lock{}, savelock{};
+    std::string line = "1,-15.5,Słoń,42,,,";
+    const char *buf = line.c_str();
 
     fd = open("file.txt", O_RDWR);
     lock.l_type    = F_WRLCK;   /* Test for any lock on any part of file. */
     lock.l_start   = 0;
-    lock.l_whence  = SEEK_SET;
-    lock.l_len     = 0;
+    lock.l_whence  = SEEK_END;
+    lock.l_len     = sizeof(line);
     savelock = lock;
-    fcntl(fd, F_GETLK, &lock);  /* Overwrites lock structure with preventors. */
-    if (lock.l_type == F_WRLCK)
-    {
-        printf("Process %d has a write lock already!\n", lock.l_pid);
-        exit(1);
+    
+    printf("trying to get lock..\n");
+    int res = (fcntl(fd,F_SETLKW,&savelock));
+    if(res==-1)
+    {     
+        fcntl(fd,F_GETLK,&lock);
+        printf("\nFile already locked by process (pid):    \t%d\n",lock.l_pid);
+        printf("%d, %ld, %ld, %d\n", lock.l_type, lock.l_start, lock.l_len, lock.l_whence);   
+        return -1;        
     }
-    else if (lock.l_type == F_RDLCK)
+    printf("locked\n");
+    if((lseek(fd,0,SEEK_END))==-1)
     {
-        printf("Process %d has a read lock already!\n", lock.l_pid);
-        exit(1);
+            perror("lseek");
+            exit(1);
     }
-    else
-        fcntl(fd, F_SETLK, &savelock);
-    pause();
+    if((write(fd, buf, sizeof(line)))==-1)
+    {
+            perror("write");
+            exit(1);
+    }
+    printf("press enter to release lock\n");
+    getchar();
+    savelock.l_type = F_UNLCK;
+    savelock.l_whence = SEEK_SET;
+    savelock.l_start = 0;
+    savelock.l_len = 0;
+    if((fcntl(fd,F_UNLCK,&savelock))==-1)
+    {
+            perror("fcntl");
+            exit(0);
+    }
+    printf("Unlocked\n");
+    close(fd);
+    return 0;
 }
