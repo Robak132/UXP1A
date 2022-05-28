34b1e8f5b31bf91364bccc67c768211afdfb263c
Paweł Müller <pawelmuller42@gmail.com>
2022-05-28 20:15:06 +0200

    Prototyped parser tests.

---
+++ tests/ParserTest.cpp
@@ -0,0 +1,11 @@
+#include "../lib/include/Parser.h"
+#include "catch.hpp"
+
+TEST_CASE("Lexer") {
+    SECTION("Test 1") {
+        REQUIRE(0 == 0);
+    }
+    SECTION("Test 2") {
+        REQUIRE(0 == 0);
+    }
+}
