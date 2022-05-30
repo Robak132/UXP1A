f764672f0ab85b917a046b6f0b42355d9024226d
Paweł Müller <pawelmuller42@gmail.com>
2022-05-30 22:04:11 +0200

    Added more tests for toCSV() method.

--- tests/TupleToCSVTest.cpp
+++ tests/TupleToCSVTest.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 
 TEST_CASE("Check toCSV()") {
-    SECTION("Test 1") {
+    SECTION("Test 1 - simple ventities") {
         Tuple tuple = Tuple(std::vector<Entity>{
                 Entity::createIntEntity(1),
                 Entity::createStringEntity("2"),
@@ -14,7 +14,7 @@ TEST_CASE("Check toCSV()") {
         std::string correctCSV = "1,\"2\",3.1,4";
         REQUIRE(result == correctCSV);
     }
-    SECTION("Test 2") {
+    SECTION("Test 2 - large entities") {
         Tuple tuple = Tuple(std::vector<Entity>{
                 Entity::createIntEntity(1),
                 Entity::createStringEntity("How much wood would a woodchuck chuck?"),
@@ -26,6 +26,18 @@ TEST_CASE("Check toCSV()") {
 
         std::string result = tuple.toCSV();
         std::string correctCSV = R"(1,"How much wood would a woodchuck chuck?","2,200","Defenestration",3.14159,40000000)";
+        REQUIRE(result == correctCSV);
+    }
+    SECTION("Test 3 - string escaping") {
+        Tuple tuple = Tuple(std::vector<Entity>{
+                Entity::createIntEntity(1),
+                Entity::createStringEntity("How much wood \"would\" a woodchuck chuck?"),
+                Entity::createStringEntity("2,200\\\\"),
+                Entity::createStringEntity("Defenestration\n"),
+        });
+
+        std::string result = tuple.toCSV();
+        std::string correctCSV = "1,\"How much wood \"would\" a woodchuck chuck?\",\"2,200\\\\\",\"Defenestration\n\"";
         REQUIRE(result == correctCSV);
     }
 }
