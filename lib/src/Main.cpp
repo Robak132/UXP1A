aafcbdfbcb0a8833fc1db6197e30ef542505eefe
Jakub Robaczewski <kubarob13@gmail.com>
2022-05-30 20:03:53 +0200

    Fixed lindatest

--- lib/src/Main.cpp
+++ lib/src/Main.cpp
@@ -5,27 +5,24 @@
 #include "../include/Parser.h"
 
 int main(int argc, char *argv[]) {
-    Linda linda = Linda();
+    Linda linda = Linda("../resources/tuples.csv", "../resources/sleeping.csv");
 
     switch(argc) {
         case 3:
             if (strcmp(argv[1], "-o") == 0) {
-                std::string stringToParse = argv[2];
-                //TODO Output
+                linda.output(argv[2]);
             } else {
                 std::cout << "Wrong syntax" << std::endl;
                 exit(0);
             }
             break;
         case 5:
-            if (strcmp(argv[1], "-i") == 0 && strcmp(argv[1], "-t") == 0) {
-                std::string stringToParse = argv[2];
-                int timeout = std::stoi(argv[4]);
-                //TODO Input
-            } else if (strcmp(argv[1], "-r") == 0 && strcmp(argv[1], "-t") == 0) {
-                std::string stringToParse = argv[2];
-                int timeout = std::stoi(argv[4]);
-                //TODO Read
+            if (strcmp(argv[1], "-i") == 0 && strcmp(argv[3], "-t") == 0) {
+                Tuple* result = linda.input(argv[2], std::stoi(argv[4]));
+                std::cout<<result->toCSV()<<std::endl;
+            } else if (strcmp(argv[1], "-r") == 0 && strcmp(argv[3], "-t") == 0) {
+                Tuple* result = linda.read(argv[2], std::stoi(argv[4]));
+                std::cout<<result->toCSV()<<std::endl;
             } else {
                 std::cout << "Wrong syntax" << std::endl;
                 exit(0);
