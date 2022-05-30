7162ee4c916d8e4c3b0ea50daae6c987fb1bbcc4
Jakub Robaczewski <kubarob13@gmail.com>
2022-05-30 20:24:25 +0200

    Expanded lindatest

--- lib/src/Main.cpp
+++ lib/src/Main.cpp
@@ -5,23 +5,73 @@
 #include "../include/Parser.h"
 
 int main(int argc, char *argv[]) {
-    Linda linda = Linda("../resources/tuples.csv", "../resources/sleeping.csv");
-
     switch(argc) {
-        case 3:
-            if (strcmp(argv[1], "-o") == 0) {
-                linda.output(argv[2]);
+        case 4:
+            if (strcmp(argv[2], "-o") == 0) {
+                // <data> -o <tuple>
+                Linda linda = Linda(argv[1]);
+                linda.output(argv[3]);
+            } else if (strcmp(argv[2], "-i") == 0) {
+                // <data> -i <template>
+                Linda linda = Linda(argv[1]);
+                Tuple* result = linda.input(argv[3], 0);
+                std::cout<<result->toCSV()<<std::endl;
+            } else if (strcmp(argv[2], "-r") == 0) {
+                // <data> -r <template>
+                Linda linda = Linda(argv[1]);
+                Tuple* result = linda.read(argv[3], 0);
+                std::cout<<result->toCSV()<<std::endl;
             } else {
                 std::cout << "Wrong syntax" << std::endl;
                 exit(0);
             }
             break;
         case 5:
-            if (strcmp(argv[1], "-i") == 0 && strcmp(argv[3], "-t") == 0) {
-                Tuple* result = linda.input(argv[2], std::stoi(argv[4]));
+            if (strcmp(argv[3], "-o") == 0) {
+                // <data> <processes> -o <tuple>
+                Linda linda = Linda(argv[1], argv[2]);
+                linda.output(argv[3]);
+            } else if (strcmp(argv[3], "-i") == 0) {
+                // <data> <processes> -i <tuple>
+                Linda linda = Linda(argv[1], argv[2]);
+                Tuple* result = linda.input(argv[4], 0);
                 std::cout<<result->toCSV()<<std::endl;
-            } else if (strcmp(argv[1], "-r") == 0 && strcmp(argv[3], "-t") == 0) {
-                Tuple* result = linda.read(argv[2], std::stoi(argv[4]));
+            } else if (strcmp(argv[3], "-r") == 0) {
+                // <data> <processes> -r <tuple>
+                Linda linda = Linda(argv[1], argv[2]);
+                Tuple* result = linda.read(argv[4], 0);
+                std::cout<<result->toCSV()<<std::endl;
+            } else {
+                std::cout << "Wrong syntax" << std::endl;
+                exit(0);
+            }
+            break;
+        case 6:
+            if (strcmp(argv[2], "-i") == 0 && strcmp(argv[4], "-t") == 0) {
+                // <data> -i <tuple> -t <timeout>
+                Linda linda = Linda(argv[1]);
+                Tuple* result = linda.input(argv[3], std::stoi(argv[5]));
+                std::cout<<result->toCSV()<<std::endl;
+            } else if (strcmp(argv[2], "-r") == 0 && strcmp(argv[4], "-t") == 0) {
+                // <data> -r <tuple> -t <timeout>
+                Linda linda = Linda(argv[1]);
+                Tuple* result = linda.read(argv[3], std::stoi(argv[5]));
+                std::cout<<result->toCSV()<<std::endl;
+            } else {
+                std::cout << "Wrong syntax" << std::endl;
+                exit(0);
+            }
+            break;
+        case 7:
+            if (strcmp(argv[3], "-i") == 0 && strcmp(argv[5], "-t") == 0) {
+                // <data> <processes> -i <tuple> -t <timeout>
+                Linda linda = Linda(argv[1], argv[2]);
+                Tuple* result = linda.input(argv[4], std::stoi(argv[6]));
+                std::cout<<result->toCSV()<<std::endl;
+            } else if (strcmp(argv[3], "-r") == 0 && strcmp(argv[5], "-t") == 0) {
+                // <data> <processes> -r <tuple> -t <timeout>
+                Linda linda = Linda(argv[1], argv[2]);
+                Tuple* result = linda.read(argv[4], std::stoi(argv[6]));
                 std::cout<<result->toCSV()<<std::endl;
             } else {
                 std::cout << "Wrong syntax" << std::endl;
